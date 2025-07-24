1812f94aba8d535cc9fbfc7cefa002d885bf8c4e
CookieWorkProject <cookieworkproject@gmail.com>
2025-07-23 20:15:37 -0400

    added one way blocks

---
+++ README.md
@@ -0,0 +1,2 @@
+# CollisionDetection
+demo of collision
---
+++ objects/BlockRight.py
@@ -0,0 +1,11 @@
+import pygame
+from objects.BasicObject import BasicObject
+class BlockRight(BasicObject):
+    def __init__(self,*args):
+        super().__init__(*args)
+        self.font = pygame.font.SysFont('Times New Roman', 16)
+        
+        
+    def draw(self,surface):
+        pygame.draw.rect(surface, (32, 32, 196), self.rect)
+        self.drawText(surface,">",(255, 255, 255),self.getCenterX(),self.getCenterY())
